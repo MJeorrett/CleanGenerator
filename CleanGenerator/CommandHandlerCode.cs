1ede388e2c5801f36d33435c9021997ee27a86d8
Matthew Jeorrett <matthew@clearskylogic.com>
2023-01-10 21:59:04 +0000

    working tt example

---
+++ CleanGenerator/CommandHandlerCode.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanGenerator;
+
+public partial class CommandHandler
+{
+    private readonly string m_testData;
+
+    public CommandHandler(string testData)
+    {
+        m_testData = testData;
+    }
+}
---
+++ CleanGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CleanGenerator;
+
+Console.WriteLine("Hello, World!");
+
+GenerateTemplate();
+
+static void GenerateTemplate()
+{
+    var test = new CommandHandler("blahem");
+    var text = test.TransformText();
+    File.WriteAllText("TestCommand.cs", text);
+}
